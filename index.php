8170baef911419bbdb69a12e27472233f02d92d9
Pape Moussa SENE <PMS.pro18@gmail.com>
2023-06-14 01:19:05 +0000

    Mise en place du code html de la page d'accueil et début stylisation du header

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="medias-site/logoDeukSil.png" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+    <title>Deuk Sil : Agence Immobilière</title>
+</head>
+<body>
+    <header>
+        <a href="index.php"><img src="medias-site/logoDeukSil.png" alt="logo"></a>
+        <form action="" method="POST" class="search-bar">
+            <input type="text" name="search" id="search" placeholder="Un Lieu?">
+            <button type="submit">Rechercher</button>
+        </form>
+        <button type="button">Se connecter?</button>
+    </header>
+    <div class="main">
+        <div class="degrade">
+        </div>
+        <section>
+            <h1>Dalal ak Jaam !!</h1>
+            <p>Notre Agence vous permet de rendre disponible votre propriété à la location. Ou de, vous même, trouver une location</p>
+            <button>Ajouter ma propriété</button>
+            <a href="locations.php"><button>Trouver une location</button></a>
+        </section>
+    </div>
+</body>
+</html>
